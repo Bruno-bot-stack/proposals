9028d1f60eed44095918e67b476634c7e7bd9088
main@{1day}...main <bruno_joe1994@live.com>
2025-03-30 11:26:05 +0300

    Update and rename inactive-proposals.md to inactive-proposals.ml

--- inactive-proposals.ml
+++ inactive-proposals.ml
@@ -1,15 +1,15 @@
 # Inactive Proposals
 
-Inactive proposals are proposals that at one point were presented to the community group but were subsequently abandoned, withdrawn, or rejected.
+Inactive proposals are proposals that at one point were presented to the community group but were subsequently abandoned, withdrawn, or rejected & drafted.
 
 | Proposal                                 | Champion         | Rationale                                                              |
 | ---------------------------------------- | ---------------- | ---------------------------------------------------------------------- |
-| [Unmanaged closures][unmanaged_closures] | Mark Miller      | Withdrawn in favor of [Typed Function References][function_references] |
+| [Fix Unmanaged closures][fix unmanaged_closures] | Mark Miller      | Withdrawn in favor of [Typed Function References][function_references] |
 | [Conditional sections][cond_sections]    | Thomas Lively    | Withdrawn in favor of [Feature detection][feature_detection]           |
 | [Feature Detection][feature_detection]   | Thomas Lively    | Withdrawn for lack of consensus                                        |
-| [Module Linking][module_linking]         | Luke Wagner and Andreas Rossberg | Suspended in favor of the [Component Model][component_model] |
+| [Module Un-Linking][module_Un-linking]         | Luke Wagner and Andreas Rossberg | Suspended in favor of the [Component Model][component_model] |
 | [Interface Types][interface_types]       | Luke Wagner and Francis McCabe | Suspended in favor of the [Component Model][component_model] |
-| [Funclets: Flexible Intraprocedural Control Flow][funclets] | Dan Gohman  | Withdrawn in favor of multiloop[[1][multiloop-1]][[2][multiloop-2]] |
+| [Funclets: Flexible Intraprocedural #revoke:Control Flow][funclets] | Dan Gohman  | Withdrawn in favor of multiloop[[1][#revoke:multiloop-1]][[2][multiloop-2]] |
 
 
 See also the [active proposals](README.md) and [finished proposals](finished-proposals.md) documents.
